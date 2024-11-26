beff0ca548072ffab5d26b8ab851ede40f6149aa
Semin Bae <semin.bae.1999@gmail.com>
2024-11-27 02:01:38 +0900

    Navbar --

--- tothemarschart/src/pages/2.js/0.Navbar.js
+++ tothemarschart/src/pages/2.js/0.Navbar.js
@@ -5,28 +5,29 @@ import Trending from "./2.Trending";
 import MyFavorite from "./3.MyFavorite";
 import Search from "./4.Search";
 import Login from "./5.Login";
-import "../1.styling/0.Navbar.css"; 
+import "../1.styling/0.Navbar.css";
 
 const Navbar = () => {
   return (
     <Router>
       <div>
         <nav className="navbar">
+          <ul className="navbar-item" id="home-icon">
+            <Link to="/"><img src="/resources/0.Navbar/Home.png" alt="Home" style={{display: "flex", width:'200px', margin:'auto', padding:'auto'}}/></Link>
+          </ul>
           <ul className="navbar-list">
-            <li className="navbar-item">
-              <Link to="/">Home</Link>
-            </li>
             <li className="navbar-item">
               <Link to="/trending">Trending</Link>
             </li>
             <li className="navbar-item">
               <Link to="/myfavorite">My Favorite</Link>
             </li>
-            <li className="navbar-item">
-              <Link to="/search">Search</Link>
+            <li className="navbar-item search-bar">
+              <input type="text" placeholder="Search" className="search-field" style={{display: "flex", width:'250px'}}/>
+              <Link to="/search" className="search-icon"><img src="/resources/0.Navbar/Vector.png" alt="Home" style={{display: "flex", width:"15px"}}/></Link>
             </li>
-            <li className="navbar-item">
-              <Link to="/login">Login</Link>
+            <li className="navbar-item" >
+              <Link to="/login" id="login">Login</Link>
             </li>
           </ul>
         </nav>
